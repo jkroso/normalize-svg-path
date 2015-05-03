2f4d255db46e8b3f5d4160a217fbf337607f36d2
Jake Rosoman <jkroso@gmail.com>
2015-05-03 21:21:44 +1200

    tabs -> spaces

--- test/normalize.test.js
+++ test/normalize.test.js
@@ -6,19 +6,19 @@ var eql = require('equals')
 var str = JSON.stringify
 
 describe('normalize', function(){
-	Object.keys(cases).forEach(function(key){
-		var code = cases[key].reduce(function(str, obj){
-			if (!obj.to) return str
-			return str 
-				+ 'var from = parse("' + obj.from + '")\n'
-				+ 'var to = parse("' + obj.to + '")\n'
-				+ (obj.debug ? 'debugger\n' : '')
-				+ 'assert(normalize(from), to)\n'
-		}, '(function(){\n') + '})'
-		it(key, eval(code))
-	})
+  Object.keys(cases).forEach(function(key){
+    var code = cases[key].reduce(function(str, obj){
+      if (!obj.to) return str
+      return str
+        + 'var from = parse("' + obj.from + '")\n'
+        + 'var to = parse("' + obj.to + '")\n'
+        + (obj.debug ? 'debugger\n' : '')
+        + 'assert(normalize(from), to)\n'
+    }, '(function(){\n') + '})'
+    it(key, eval(code))
+  })
 })
 
 function assert(a, b){
-	if (!eql(a, b)) throw new Error(str(a) + ' != ' + str(b))
+  if (!eql(a, b)) throw new Error(str(a) + ' != ' + str(b))
 }
